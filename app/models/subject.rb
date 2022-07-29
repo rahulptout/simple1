14204d51f36fde6ebc5df88f49a38286c4055410
rahul <rahulp.tout@gmail.com>
2022-07-29 10:54:20 +0530

    3th day In Progress

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,10 @@
 class Subject < ApplicationRecord
+  has_one :page
+
   scope :visible,lambda {where(visible: true )}
-  scope :invisible, lambda {where(:visible => false )}
+  scope :invisible, lambda {where(visible: false )}
   scope :sorted, lambda {order("position ASC")}
   scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
+
 end
+ 
